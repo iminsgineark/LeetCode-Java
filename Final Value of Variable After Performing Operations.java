fcc87b068f0106772d3437400b0634c8df8b683a
Utkrist Ark <92156636+iminsgineark@users.noreply.github.com>
2023-06-22 17:15:39 +0530

    Update Final Value of Variable After Performing Operations.java

--- Final Value of Variable After Performing Operations.java
+++ Final Value of Variable After Performing Operations.java
@@ -1,1 +1,8 @@
-
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for(String str : operations)
+            x += str.charAt(1) == '+' ? 1 : -1;
+        return x;
+    }
+}
