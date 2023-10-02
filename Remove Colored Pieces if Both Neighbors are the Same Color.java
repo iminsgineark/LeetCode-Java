95e36a42b192a9c1ae31edee101ddcdc8e597576
Utkrist Ark <92156636+iminsgineark@users.noreply.github.com>
2023-10-02 06:45:28 +0530

    Update Remove Colored Pieces if Both Neighbors are the Same Color.java

--- Remove Colored Pieces if Both Neighbors are the Same Color.java
+++ Remove Colored Pieces if Both Neighbors are the Same Color.java
@@ -1,1 +1,20 @@
 
+class Solution {
+    public boolean winnerOfGame(String colors) {
+        boolean result = false;
+        int ACount = ConsecutiveString(colors,'A');
+        int BCount = ConsecutiveString(colors,'B');
+        return ACount > BCount;
+    }
+    private int ConsecutiveString(String s,char ch){
+        int c = 0;
+        int i = 1;
+        while(i < s.length() - 1){
+            if(s.charAt(i) == ch && s.charAt( i -1) == s.charAt(i) && s.charAt(i) == s.charAt(i+1)){
+                c++;
+            }
+            i++;
+        }
+        return c;
+    }
+}
