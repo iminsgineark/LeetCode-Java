d423b7733cc426dfb601e8b9455479ff1daa958c
Utkrist Ark <92156636+iminsgineark@users.noreply.github.com>
2023-10-05 11:14:45 +0530

    Update MajorityElement.java

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,7 +1,36 @@
+
+/**
+ * @author Utkrist Ark
+ * @date 20-05-2023
+ * @version java 11.0.19
+ */
+
+
+
+package Arrays;
+
 import java.util.Arrays;
-class Solution {
+class Solution5 {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+
+        // Approach 1
+//        Arrays.sort(nums);
+//        return nums[nums.length / 2];
+
+        //Striver's Method(Optimal Solution)
+        int count = 0;
+        int candidate = 0;
+        for (int num :
+                nums) {
+            if (count == 0){
+                candidate = num;
+            } else if (num == candidate) {
+                count = count + 1;
+            }
+            else {
+                count = count - 1;
+            }
+        }
+        return candidate;
     }
 }
